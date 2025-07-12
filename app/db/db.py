f648b2c15dacf2a80cacc37d5f7baf95fab0ca70
whshin <urihiya1@kaist.ac.kr>
2025-07-12 10:55:59 +0900

    1055

--- app/db/db.py
+++ app/db/db.py
@@ -4,10 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{os.getenv('PGUSER')}:{os.getenv('PGPASSWORD')}"
-    f"@{os.getenv('PGHOST')}:{os.getenv('PGPORT')}/{os.getenv('PGDATABASE')}"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
