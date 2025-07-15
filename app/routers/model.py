764fa520fe1d083c334a20ba4afc8380dbb2dcca
whshin <urihiya1@kaist.ac.kr>
2025-07-15 22:31:30 +0900

    JULY 15 2231

--- app/routers/model.py
+++ app/routers/model.py
@@ -30,54 +30,6 @@ def get_recommendation_from_text(text: str=Form(...), r:int=Form(...), g:int=For
                                 db: Session=Depends(get_db), user: User=Depends(get_current_user)):
     
     return None
-    style_embedding, color = get_text_embedding_with_rgb(text, r, g, b) # "scandinavian", int, int, int -> 
-    prompt = f"{color} colored {text} style "
-    bad_objects = analyze_bad_objects_with_text(image=image, prompt=prompt)
-
-    recommendations = {}
-    used_ids = set()
-
-    for label in bad_objects:
-        prompt = f"{color} colored {text} style {label}"
-        prompt_embedding = get_text_embedding(prompt)
-
-        top_k_products = (
-            db.query(Product)
-            .filter(Product.embedding != None)
-            .order_by(func.cosine_distance(Product.embedding, prompt_embedding))
-            .limit(6)
-            .all()
-        )
-
-        used_ids.update(p.id for p in top_k_products)
-        recommendations[label] = [
-            ProductResponse(**p.__dict__, user_saves=False) for p in top_k_products
-        ]
-
-    top_k_etc = (
-        db.query(Product)
-        .filter(Product.embedding != None, ~Product.id.in_(used_ids))
-        .order_by(func.cosine_distance(Product.embedding, style_embedding))
-        .limit(30) 
-        .all()
-    )
-
-    top_k_filtered = []
-    for p in top_k_etc:
-        sim = cosine_similarity(
-            np.array([style_embedding]),
-            np.array([p.embedding])
-        )[0][0]
-        if sim > threshold_text:
-            top_k_filtered.append(p)
-        if len(top_k_filtered) >= 10:
-            break
-
-    recommendations["기타"] = [
-        ProductResponse(**p.__dict__, user_saves=False) for p in top_k_filtered
-    ]
-
-    return recommendations
 
 @router.post("/recommend/image", response_model=dict)
 def get_recommendation_from_image(
@@ -87,52 +39,3 @@ def get_recommendation_from_image(
     user = Depends(get_current_user)
 ):
     return None
-    feed_image = db.query(FeedImage).filter(FeedImage.id == image_id).first()
-    if not feed_image or feed_image.embedding is None:
-        raise HTTPException(status_code=404, detail="Not Found")
-
-    wish_embedding = feed_image.embedding
-
-    if image:
-        bad_objects = analyze_bad_objects_with_image(image=image, embedding=wish_embedding)
-    else:
-        bad_objects = []
-
-    recommendations = {}
-    used_ids = set()
-
-    for label in bad_objects:
-        top_k_products = (
-            db.query(Product)
-            .filter(Product.embedding != None)
-            .order_by(func.cosine_distance(Product.embedding, wish_embedding))
-            .limit(6)
-            .all()
-        )
-        used_ids.update(p.id for p in top_k_products)
-        recommendations[label] = [
-            ProductResponse(**p.__dict__, user_saves=False) for p in top_k_products
-        ]
-
-    top_k_etc = (
-        db.query(Product)
-        .filter(Product.embedding != None, ~Product.id.in_(used_ids))
-        .order_by(func.cosine_distance(Product.embedding, wish_embedding))
-        .limit(30)
-        .all()
-    )
-
-    top_k_filtered = []
-    for p in top_k_etc:
-        sim = cosine_similarity(
-            np.array([wish_embedding]),
-            np.array([p.embedding])
-        )[0][0]
-        if sim > threshold_image:
-            top_k_filtered.append(p)
-        if len(top_k_filtered) >= 6:
-            break
-    recommendations["기타"] = [
-        ProductResponse(**p.__dict__, user_saves=False) for p in top_k_filtered
-    ]
-    return recommendations
